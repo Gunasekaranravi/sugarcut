d16f506454a4a6fc9c310f7bb63afc91ff2ce451
Gunasekaranravi <gunasekaranrvi15@gmail.com>
2025-08-28 18:04:13 +0530

    second

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sugarcut/',
+  base: './', // Changed from '/sugarcut/' to './'
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
